83adc96ed385df22f514c852dd307a41ee88b003
Andreas Steiner <96056736+rep0mancer@users.noreply.github.com>
2025-08-12 14:28:48 +0200

    Delete engine directory

--- engine/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './types';
-export * from './bands';
-export * from './stacking';
-export * from './sequence';
-export * from './packer';
-export * from './height';
-export * from './axles';
